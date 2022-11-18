c11f6ba6cc00b3e058293af496bd0070bc78d2cf
Iminguet <i.minguet@hotmail.com>
2022-11-18 09:26:43 +0100

    en busca del arca perdida

--- buscaminas/js/main.js
+++ buscaminas/js/main.js
@@ -3,15 +3,40 @@ const value = document.getElementById('value');
 const tagHtml = document.getElementById('tagHtml');
 const name = document.getElementById('name');
 
-const elementoClickado = document.getElementsByClassName('tapa');
+const cuadrados = document.getElementsByClassName('cuadrado');
+
+// const elementoClickado = document.getElementsByClassName('tapa');
+
+const arrayTapas = [...cuadrados];
+
+// console.log(arrayTapas);
+
+const añadirTapa = (arrayCuadrados) => {
+  // console.log(arrayCuadrados);
+  let arrayTapados = [];
+  arrayTapados = arrayCuadrados.map((cuadrado) => {
+    // console.log(cuadrado, '1');
+    const tapa = document.createElement('div');
+    tapa.classList.add('tapa');
+    cuadrado.appendChild(tapa);
+  });
+  console.log(arrayTapados);
+  return arrayTapados;
+};
 
 const obtenerValor = (arrayValores) => {
+  console.log(arrayValores);
   for (const valor of arrayValores) {
-    valor.addEventListener('click', (e) => {
-      console.log(valor);
+    console.log(valor);
+    valor.addEventListener('click', (event) => {
+      event.preventDefault();
+      selector.innerHTML = valor.classList;
+      value.innerHTML = valor;
+      tagHtml.innerHTML = valor.tagName;
+      // console.table(valor.classList);
       valor.classList.add('selected');
     });
   }
 };
 
-obtenerValor(elementoClickado);
+obtenerValor(añadirTapa(arrayTapas));
