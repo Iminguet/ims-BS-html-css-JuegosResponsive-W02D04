daf47cbee91de66c57e732eb66ad56120b2fd493
Iminguet <i.minguet@hotmail.com>
2022-11-21 18:27:51 +0100

    conecta 4 terminado. Falta lógica de juego

--- conecta4/js/main.js
+++ conecta4/js/main.js
@@ -1,14 +1,10 @@
-const selector = document.getElementsByClassName('selector');
+const selector = document.getElementById('selector');
 const value = document.getElementById('value');
 const tagHtml = document.getElementById('tagHtml');
 const nameId = document.getElementById('name');
 const espacioDeJuego = document.getElementsByClassName('espacioJuego');
 
-// const elementoClickado = document.getElementsByClassName('tapa');
-
 const arrayElementosJuego = [...espacioDeJuego];
-
-// console.log(arrayTapas);
 
 const obtenerValor = (arrayValores) => {
   console.log(arrayValores);
@@ -16,13 +12,17 @@ const obtenerValor = (arrayValores) => {
     valor.classList.add('noPlay');
     valor.addEventListener('click', (event) => {
       event.preventDefault();
-      value.innerHTML = valor;
-      tagHtml.innerHTML = valor.tagName;
-      nameId.innerHTML = valor.id;
       valor.classList.add('playedPlayer01');
-      selector.innerHTML = valor.classList;
+      pintarValores(valor);
     });
   }
+};
+
+const pintarValores = (valor) => {
+  value.innerHTML = valor;
+  tagHtml.innerHTML = valor.tagName;
+  nameId.innerHTML = valor.id;
+  selector.innerHTML = valor.classList;
 };
 
 obtenerValor(arrayElementosJuego);
